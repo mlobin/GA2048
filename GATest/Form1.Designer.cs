2f4df5d85e0e49c71d5649b99c2c35e56c92217f
m.lobin <m.lobin@list.ru>
2019-08-17 03:14:23 +0300

    Initial commit. Failed(?) attempt to make genetic algorithm play 2048.

---
+++ GATest/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace GATest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(78, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(140, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(210, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(290, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(78, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(140, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 24);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "label2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(210, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 24);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "label3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(290, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 24);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(78, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 24);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "label1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(140, 149);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 24);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "label2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(210, 149);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 24);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "label3";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(290, 149);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 24);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "label4";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(78, 195);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 24);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "label1";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(140, 195);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(60, 24);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "label2";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(210, 195);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(60, 24);
+            this.label15.TabIndex = 2;
+            this.label15.Text = "label3";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label16.Location = new System.Drawing.Point(290, 195);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 24);
+            this.label16.TabIndex = 3;
+            this.label16.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 265);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ GATest/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GATest
+{
+    public partial class Form1 : Form
+    {
+        List<List<Label>> field = new List<List<Label>>();
+        //List<List<int>> fieldInt = new List<List<int>>();
+        List<Player> players = new List<Player>();
+        public Form1()
+        {
+
+            InitializeComponent();
+            for (int i = 0; i < 4; i++)
+            {
+                List<int> tmpInt = new List<int>();
+                for (int j = 0; j < 4; j++)
+                    tmpInt.Add(0);
+                //fieldInt.Add(tmpInt);
+                field.Add(new List<Label>());
+            }
+            field[0].Add(label1);
+            field[0].Add(label2);
+            field[0].Add(label3);
+            field[0].Add(label4);
+            field[1].Add(label5);
+            field[1].Add(label6);
+            field[1].Add(label7);
+            field[1].Add(label8);
+            field[2].Add(label9);
+            field[2].Add(label10);
+            field[2].Add(label11);
+            field[2].Add(label12);
+            field[3].Add(label13);
+            field[3].Add(label14);
+            field[3].Add(label15);
+            field[3].Add(label16);
+            List<List<int>> fieldInt = new List<List<int>>();
+            for (int cnt = 0; cnt < 4; cnt++)
+            {
+                fieldInt.Add(new List<int>());
+                for (int cnt1 = 0; cnt1 < 4; cnt1++)
+                {
+                    fieldInt[cnt].Add(0);
+                }
+            }
+            drawField(fieldInt);
+
+        }
+
+        public static List<List<int>> AddTile(List<List<int>> fieldInt)
+        {
+            Random r = new Random();
+            int next = r.Next(16);
+            int cnt = 0;
+            List<int> n = new List<int>();
+            for (int i = 0; i < 16; i++)
+            {
+                if (fieldInt[i / 4][i % 4] == 0) n.Add(i);
+            }
+            cnt = n[r.Next(n.Count)];
+            fieldInt[cnt/4][cnt%4] = r.Next(100) <= 20 ? 4 : 2;
+            return fieldInt;
+        }
+
+        public void drawField(List<List<int>> fieldInt)
+        {
+            for (int i = 0; i < 16; i++)
+                field[i / 4][i % 4].Text = fieldInt[i / 4][i % 4].ToString();
+        }
+
+        public static List<List<int>> MoveTL(int pos, List<List<int>> fieldInt, bool show=false, Form1 f = null)
+        {
+            
+             switch (pos)
+            {
+                case 1:
+                    for (int i = 0; i < 4; i++)
+                        for (int j = 1; j < 4; j++)
+                        {
+                            int t = j;
+                            if (fieldInt[i][j] != 0)
+                            {
+                                while (t > 0 && fieldInt[i][t-1] == 0)
+                                    t--;
+                                if (t != j)
+                                {
+                                    fieldInt[i][t] = fieldInt[i][j];
+                                    fieldInt[i][j] = 0;
+                                }
+                                if (t>0 && fieldInt[i][t - 1] == fieldInt[i][t])
+                                    {
+                                        fieldInt[i][t - 1] = fieldInt[i][t - 1] * 2;
+                                        fieldInt[i][t] = 0;
+                                    }
+                            }
+                        }
+                    break;
+                case 2:
+                    for (int j = 0; j < 4; j++)
+                        for (int i = 2; i >=0; i--)
+                        {
+                            int t = i;
+                            if (fieldInt[i][j] != 0)
+                            {
+                                while (t < 3 && fieldInt[t+1][j] == 0)
+                                    t++;
+                                if (t != i)
+                                {
+                                    fieldInt[t][j] = fieldInt[i][j];
+                                    fieldInt[i][j] = 0;
+                                }
+                                if (t < 3 && fieldInt[t+1][j] == fieldInt[t][j])
+                                {
+                                    fieldInt[t+1][j] = fieldInt[t+1][j] * 2;
+                                    fieldInt[t][j] = 0;
+                                }
+                            }
+                        }
+                    break;
+                case 3:
+                    for (int i = 1; i < 4; i++)
+                        for (int j = 0; j < 4; j++)
+                        {
+                            int t = i;
+                            if (fieldInt[i][j] != 0)
+                            {
+                                while (t > 0 && fieldInt[t-1][i] == 0)
+                                    t--;
+                                if (t != i)
+                                {
+                                    fieldInt[t][j] = fieldInt[i][j];
+                                    fieldInt[i][j] = 0;
+                                }
+                                if (t > 0 && fieldInt[t-1][j] == fieldInt[t][j])
+                                {
+                                    fieldInt[t-1][j] = fieldInt[t-1][j] * 2;
+                                    fieldInt[t][j] = 0;
+                                }
+                            }
+                        }
+
+                    break;
+                case 4:
+                    for (int i = 0; i < 4; i++)
+                        for (int j = 2; j >=0; j--)
+                        {
+                            int t = j;
+                            if (fieldInt[i][j] != 0)
+                            {
+                                while (t < 3 && fieldInt[i][t+1] == 0)
+                                    t++;
+                                if (t != j)
+                                {
+                                    fieldInt[i][t] = fieldInt[i][j];
+                                    fieldInt[i][j] = 0;
+                                }
+                                if (t<3 && fieldInt[i][t + 1] == fieldInt[i][t])
+                                    {
+                                        fieldInt[i][t + 1] = fieldInt[i][t + 1] * 2;
+                                        fieldInt[i][t] = 0;
+                                    }
+                            }
+                        }
+                    break;
+            }
+                fieldInt = AddTile(fieldInt);
+
+            if (show) f.drawField(fieldInt);
+            return fieldInt;
+
+        }
+
+        public void KillAndBreed()
+        {
+            Random r = new Random();
+            List<Player> t = new List<Player>(players);
+            while (t.Count > 20)
+            {
+                int a = r.Next(t.Count), b = r.Next(t.Count);
+                while (a==b)
+                    a = r.Next(t.Count);
+                if (t[a].score > t[b].score)
+                    t.RemoveAt(b);
+                else t.RemoveAt(a);
+            }
+            while (t.Count < 100)
+            {
+                int a = r.Next(players.Count), b = r.Next(players.Count);
+                while (a == b)
+                    a = r.Next(players.Count);
+                t.Add(Player.Breed(players[a], players[b]));
+            }
+            for (int i = 0;i<100;i++)
+            {
+                if (r.Next(100) > 79)
+                    t[i].Mutate();
+            }
+            players = new List<Player>(t);
+        }
+
+        public void Teach()
+        {
+
+            var a = MessageBox.Show("Дальше?", "Показать результаты?", MessageBoxButtons.YesNoCancel);
+            Setup();
+            while (a!=DialogResult.Cancel)
+            {
+                for (int i = 0; i < 20;i++)
+                {
+                    KillAndBreed();
+                }
+                a = MessageBox.Show("Дальше?", "Показать результаты?", MessageBoxButtons.YesNoCancel);
+                if (a == DialogResult.Yes)
+                {
+                    int max = 0;
+                    for (int i = 0; i < 100; i++)
+                        max = max < players[i].score ? players[i].score : max;
+                    MessageBox.Show(max.ToString());
+                }
+                for (int i = 0; i < 100; i++)
+                    players[i].Add();
+            }
+        }
+        public void Setup()
+        {
+            for (int i = 0; i < 20; i++)
+                players.Add(new Player());
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /*
+            if (e.KeyCode == Keys.Left)
+                MoveTL(1,);
+            if (e.KeyCode == Keys.Down)
+                MoveTL(2);
+            if (e.KeyCode == Keys.Up)
+                MoveTL(3);
+            if (e.KeyCode == Keys.Right)
+                MoveTL(4);
+                */
+            Teach();
+        }
+    }
+
+    public class Player
+    {
+        List<int> moves;
+        public int score;
+        public Player(List<int> a)
+        {
+            moves = a;
+            int scoresum = 0;
+            for (int i = 0; i < 10; i++)
+                scoresum += GetScore();
+            score = scoresum / 10;
+        }
+
+        public Player()
+        {
+            Random r = new Random();
+            moves = new List<int>();
+            for (int i = 0; i < 100; i++)
+                moves.Add(r.Next(1, 5));
+            int scoresum = 0;
+            for (int i = 0; i < 10; i++)
+                scoresum += GetScore();
+            score = scoresum / 10;
+        }
+
+        public static Player Breed(Player p1, Player p2)
+        {
+            Random r = new Random();
+            r.Next(1, 2);
+            List<int> t = new List<int>();
+            for (int i = 0; i < p1.moves.Count && i < p2.moves.Count; i++)
+                t.Add(r.Next(1, 3) == 2 ? p1.moves[i] : p2.moves[i]);
+            return new Player(t);
+        }
+
+        public void Mutate()
+        {
+            Random r = new Random();
+            int i = moves.Count;
+            while (i-- > 0)
+                if (r.Next(1, 11) < 3)
+                    moves[i] = r.Next(1, 5);
+        }
+
+        public int GetScore()
+        {
+            List<List<int>> fieldInt = new List<List<int>>();
+            for (int cnt = 0; cnt < 4; cnt++)
+            {
+                fieldInt.Add(new List<int>());
+                for (int cnt1 = 0; cnt1 < 4; cnt1++)
+                    fieldInt[cnt].Add(0);
+            }
+            int score = 0;
+            for (int i = 0; i < moves.Count; i++)
+                try
+                { fieldInt = Form1.MoveTL(moves[i], fieldInt, false); }
+                catch { break; }
+            for (int i = 0; i < 16; i++)
+                score += fieldInt[i / 4][i % 4];
+            return score;
+        }
+
+        public void Add()
+        {
+            Random r = new Random();
+            List<int> ts = new List<int>();
+            for (int i = 10; i > 0; i--)
+                moves.Add(r.Next(1, 5));
+        }
+        
+    }
+}
